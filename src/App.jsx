04d723b416052df07c15ed1b31e5a1e37049fb0e
aswinsajan <aswinks039@gmail.com>
2024-01-12 11:28:46 +0530

    game-app

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css';
+import Cards from './Components/Cards';
+
+
+function App() {
+  return (
+    <div className="App">
+  <h1>Memory Game</h1>
+  <Cards/>
+  
+    </div> 
+  );
+}
+
+export default App;
